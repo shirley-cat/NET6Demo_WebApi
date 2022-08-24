4a9edfb02478f07a8525ba986e089028bfd97b7f
shirley <771498693@qq.com>
2022-08-24 21:07:47 +0800

    log

---
+++ NET6Demo.Utility/Log/Log4Extention.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NET6Demo.Utility.Log
+{
+    public static class Log4Extention
+    {
+        public static void InitLog4(ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.AddFilter("System", LogLevel.Warning);
+            loggingBuilder.AddFilter("Microsoft", LogLevel.Warning);//过滤掉系统默认的一些日志
+            loggingBuilder.AddLog4Net(new Log4NetProviderOptions()
+            {
+                Log4NetConfigFileName = "Config/log4net.config",
+                Watch = true
+            });
+        }
+    }
+}
